50080bec91c1b326962c0b3cc4b62b3a1e1432ce
Marc Raymond A. Serrano <marc.serrano8779@gmail.com>
2022-10-05 21:51:00 -0700

    Create CountingSort1.cs

---
+++ CountingSort1.cs
@@ -0,0 +1,61 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<int> countingSort(List<int> arr)
+    {
+        // initialize array with indices 0-99 with all zeros
+        int[] freqArr = new int[100];
+        
+        // for each of the values of the input list
+        for(int i = 0; i < arr.Count; i++)
+        {
+            // increment the frequency of that value in corresponding index in freqArr
+            freqArr[arr[i]]+=1;
+        }
+        
+        // the return type is list, so convert to a list
+        List<int> rList = freqArr.ToList();
+        return rList;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> result = Result.countingSort(arr);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
